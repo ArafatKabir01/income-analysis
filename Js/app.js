9490147c6ae6acebe60c48df3fbea760c7ad83b7
ak nabil <nabil786.ak@gmail.com>
2022-02-17 18:30:45 +0600

    updateed

--- Js/app.js
+++ Js/app.js
@@ -17,20 +17,20 @@ if (incomeValuNum >= 0 && foodExpValuNum >= 0 && rentExpValuNum >= 0 && clothesE
         if(totalExp < incomeField){
                 balancecenter.innerText = curretBalance;
                 totalExpCenter.innerText = totalExp;
-                const error = document.getElementById('error-msgs');
+                const error = document.getElementById('error-expenses-msg');
                 error.style.display='none'
                 }
         else{
-                const error = document.getElementById('error-msgs');
+                const error = document.getElementById('error-expenses-msg');
                 error.style.display='block'
                 }
-        const error = document.getElementById('error-msgs-negative');
+        const error = document.getElementById('error-negative-msg');
         error.style.display='none'
     }
    
 
 else{
-        const error = document.getElementById('error-msgs-negative');
+        const error = document.getElementById('error-negative-msg');
         error.style.display='block'
 
     }
@@ -50,11 +50,11 @@ document.getElementById('saving-btn').addEventListener('click', function(){
         if(savingInputFieldValue >= 0){
                 const savingInputFieldValueParsentage = savingInputFieldValue / 100;
                 savingAmmount.innerText =Math.floor(incomeValuNum * savingInputFieldValueParsentage);
-                const error = document.getElementById('error-msgs-negative');
+                const error = document.getElementById('error-negative-msg');
                 error.style.display='none'
         }
         else{
-                const error = document.getElementById('error-msgs-negative');
+                const error = document.getElementById('error-negative-msgs');
         error.style.display='block' 
      
         }
@@ -63,27 +63,31 @@ document.getElementById('saving-btn').addEventListener('click', function(){
         const remainingBal = balancecenterNum - parseFloat(savingAmmount.innerText);
         if (parseFloat(remainingBal) > 0){
                 remainingBalance.innerText = remainingBal;
-                const error = document.getElementById('error-msgs-saving');
+                const error = document.getElementById('error-saving-msg');
                 error.style.display='none' 
         }
         else{
-                const error = document.getElementById('error-msgs-saving');
+                const error = document.getElementById('error-saving-msg');
                 error.style.display='block' 
         }
 
 });
 
 
+function closeButton(buttonName){
+        const idCall = document.getElementById(buttonName + '-msg');
+        idCall.style.display="none";
+}
 
 
 
        // toast close button function
-document.getElementById('error-btn').addEventListener('click', function(){
-    const error = document.getElementById('error-msgs');
-        error.style.display='none'
+document.getElementById('error-exp-btn').addEventListener('click', function(){
+ closeButton('error-expenses');
 });
-
-
-// if (incomeValuNum > 0 && foodExpValuNum > 0 && rentExpValuNum > 0 && clothesExpValuNum > 0){
-
-        // && foodExpValuNum > 0 && rentExpValuNum > 0 && clothesExpValuNum > 0
+document.getElementById('error-negative-btn').addEventListener('click', function(){
+ closeButton('error-negative');
+});
+document.getElementById('error-saving-btn').addEventListener('click', function(){
+ closeButton('error-saving');
+});
